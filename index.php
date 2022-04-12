91e6f308e35354009077e8798a53ce6b998a7959
Alexandre Lira <alexandrelira137@gmail.com>
2022-04-12 20:58:01 -0300

    Criando pg de edit e pesquisa

--- index.php
+++ index.php
@@ -9,7 +9,7 @@
     <!-- Bootstrap CSS -->
     <link href="css/bootstrap.min.css" rel="stylesheet">
 
-    <title>Cadastro</title>
+    <title>HUB Saúde</title>
 </head>
 
 <body>
@@ -17,49 +17,13 @@
     <div class="container">
         <div class="row">
             <div class="col">
-                <h1>Cadastro</h1>
-                <form action="cadastro_script.php" method="POST">
-                    <div class="mb-3">
-                        <label for="nome" class="form-label">Nome completo</label>
-                        <input type="text" class="form-control" name="nome" required>
-                    </div>
-                    <div class="mb-3">
-                        <label for="profissao" class="form-label">Profissão</label>
-                        <input type="text" class="form-control" name="profissao">
-                    </div>
-                    <div class="mb-3">
-                        <label for="raca" class="form-label">Raça</label>
-                        <input type="text" class="form-control" name="raca">
-                    </div>
-                    <div class="mb-3">
-                        <label for="data_nascimento" class="form-label">Data de nascimento</label>
-                        <input type="date" class="form-control" name="data_nascimento">
-                    </div>
-                    <div class="mb-3">
-                        <label for="sexo" class="form-label">Sexo</label>
-                        <select class="form-select" aria-label="Default select example" name="sexo">
-                            <option selected >Selecione seu sexo</option>
-                            <option value="M">Masculino</option>
-                            <option value="F">Feminino</option>
-                            <option value="O">Outro</option>
-                        </select>
-                    </div>
-                    <div class="mb-3">
-                        <label for="sus" class="form-label">Número do SUS</label>
-                        <input type="number" class="form-control" name="sus">
-                    </div>
-                    <div class="mb-3">
-                        <label for="cpf" class="form-label">CPF (login)</label>
-                        <input type="number" class="form-control" name="cpf">
-                    </div>
-                    <div class="mb-3">
-                        <label for="senha" class="form-label">Senha</label>
-                        <input type="password" class="form-control" name="senha">
-                    </div>
-                    <div class="mb-3">
-                        <input class="btn btn-primary" type="submit" value="Submit">
-                    </div>
-                </form>
+                <div class="jumbotron">
+                    <h1 class="display-6">Cadastro de paciêntes</h1>
+                    <p>Acesse as funções.</p>
+                    <a class="btn btn-primary btn-lg" href="cadastro.php" role="button">Cadastrar</a>
+                    <a class="btn btn-primary btn-lg" href="pesquisa.php" role="button">Pesquisar</a>
+                </div>
+                
             </div>
         </div>
     </div>
